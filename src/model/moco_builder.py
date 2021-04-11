057dce2f98428d29f12b59ddd631676348015d66
Arthur Jinyue Guo <951224758arthur@gmail.com>
2021-04-12 02:24:33 +0800

    Fix distributed error

--- src/model/moco_builder.py
+++ src/model/moco_builder.py
@@ -167,9 +167,9 @@ def concat_all_gather(tensor):
     Performs all_gather operation on the provided tensors.
     *** Warning ***: torch.distributed.all_gather has no gradient.
     """
-    tensors_gather = [torch.ones_like(tensor)
-        for _ in range(torch.distributed.get_world_size())]
-    torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
+    # tensors_gather = [torch.ones_like(tensor)
+        # for _ in range(torch.distributed.get_world_size())]
+    # torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
 
-    output = torch.cat(tensors_gather, dim=0)
-    return output
+    # output = torch.cat(tensors_gather, dim=0)
+    return tensor

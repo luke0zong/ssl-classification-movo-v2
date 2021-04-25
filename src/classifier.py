0e3dac827c7571485fe501f48b7d4967cd35f746
Arthur Jinyue Guo <951224758arthur@gmail.com>
2021-04-26 03:41:00 +0800

    Update classifier.py

--- src/classifier.py
+++ src/classifier.py
@@ -24,7 +24,7 @@ class Classifier(pl.LightningModule):
         with torch.no_grad():
             y_hat = self.resnet(x).squeeze()
             # y_hat = nn.functional.normalize(y_hat, dim=1)
-            y_hat = nn.functional.argmax(y_hat, dim=1)
+            y_hat = nn.functional.softmax(y_hat, dim=1)
         y_hat = self.fc(y_hat)
         return y_hat
 

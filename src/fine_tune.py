3fe8f5ec8aeb267ebbbdb40308c74add9331bf1a
Arthur Jinyue Guo <951224758arthur@gmail.com>
2021-05-02 20:45:16 +0800

    Add fine-tuning

---
+++ src/fine_tune.py
@@ -0,0 +1,322 @@
+# Author: Arthur Jinyue Guo (jg5505)
+import argparse
+import builtins
+import os
+import random
+import shutil
+import time
+import warnings
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.models as models
+import torch.nn.functional as F
+
+# from model.resnet50 import Resnet50
+from dataloader import CustomDataset
+from model.subbatchnorm import SubBatchNorm2d
+
+parser = argparse.ArgumentParser(description='DL09 MoCo Fine-tuning')
+parser.add_argument('data', metavar='DIR',
+                    help='path to dataset')
+parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet50')
+parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+parser.add_argument('--epochs', default=100, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=128, type=int,
+                    metavar='N',
+                    help='mini-batch size (default: 128), this is the total '
+                         'batch size of all GPUs on the current node when '
+                         'using Data Parallel or Distributed Data Parallel')
+parser.add_argument('--lr', '--learning-rate', default=30., type=float,
+                    metavar='LR', help='initial learning rate', dest='lr')
+parser.add_argument('--schedule', default=[60, 80], nargs='*', type=int,
+                    help='learning rate schedule (when to drop lr by a ratio)')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--wd', '--weight-decay', default=0., type=float,
+                    metavar='W', help='weight decay (default: 0.)',
+                    dest='weight_decay')
+parser.add_argument('-p', '--print-freq', default=100, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('--seed', default=None, type=int,
+                    help='seed for initializing training. ')
+parser.add_argument('--gpu', default=0, type=int,
+                    help='GPU id to use.')
+parser.add_argument('--pretrained', default='', type=str,
+                    help='path to moco pretrained checkpoint')
+parser.add_argument('--eval-per-n-epoch', default=5, type=int,
+                    help='evaluate the training result per n epoch (default: 5)')
+parser.add_argument('--checkpoint-dir', default='../checkpoints', type=str, metavar='DIR',
+                    help='dir to save checkpoints')
+
+best_acc1 = 0
+
+def main():
+    args = parser.parse_args()
+
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        cudnn.deterministic = True
+        warnings.warn('You have chosen to seed training. '
+                      'This will turn on the CUDNN deterministic setting, '
+                      'which can slow down your training considerably! '
+                      'You may see unexpected behavior when restarting '
+                      'from checkpoints.')
+
+    if args.gpu is not None:
+        warnings.warn('You have chosen a specific GPU. This will completely '
+                      'disable data parallelism.')
+
+    main_worker(args.gpu, args)
+
+
+def main_worker(gpu, args):
+    global best_acc1
+    args.gpu = gpu
+
+    if args.gpu is not None:
+        print("Use GPU: {} for training".format(args.gpu))
+
+    # create model
+    print("=> creating model '{}'".format(args.arch))
+    model = models.__dict__[args.arch](num_classes=800, norm_layer=SubBatchNorm2d)
+
+
+    # load from pre-trained, before DistributedDataParallel constructor
+    if args.freeze_ckpt:
+        if os.path.isfile(args.freeze_ckpt):
+            print("=> loading freezed checkpoint '{}'".format(args.freeze_ckpt))
+            model.load_state_dict(torch.load(args.freeze_ckpt))
+
+            print("=> loading freezed checkpoint '{}'".format(args.freeze_ckpt))
+        else:
+            raise ValueError("=> no freezed checkpoint found at '{}'".format(args.freeze_ckpt))
+
+
+    if args.gpu is not None:
+        torch.cuda.set_device(args.gpu)
+        model = model.cuda(args.gpu)
+    else:
+        print("=> ERROR: gpu must be assigned")
+
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss().cuda(args.gpu)
+
+    # optimize only the linear classifier
+    optimizer = torch.optim.SGD(model.parameters(), args.lr,
+                                momentum=args.momentum,
+                                weight_decay=args.weight_decay)
+
+    cudnn.benchmark = True
+
+    # Data loading code
+    traindir = args.data
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+    transform_train = transforms.Compose([
+            transforms.RandomResizedCrop(96),  # add crop resize
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize])
+    transform_eval = transforms.Compose([
+            transforms.Resize(128),  # add resize
+            transforms.CenterCrop(96),  # add crop
+            transforms.ToTensor(),
+            normalize
+            ])
+
+    train_dataset = CustomDataset(traindir, 'train', transform_train)
+    eval_dataset = CustomDataset(traindir, 'val', transform_eval)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=True,
+        num_workers=args.workers, pin_memory=True)
+    eval_loader = torch.utils.data.DataLoader(
+        eval_dataset, batch_size=args.batch_size, shuffle=True,
+        num_workers=args.workers, pin_memory=True)
+
+    # training code
+    for epoch in range(args.start_epoch, args.epochs):
+
+        adjust_learning_rate(optimizer, epoch, args)
+
+        # train for one epoch
+        train(train_loader, model, criterion, optimizer, epoch, args)
+
+        if epoch == 0 or (epoch + 1) % args.eval_per_n_epoch == 0:
+            accuracy = evaluate(eval_loader, model, args)
+            print(f"=> Epoch: {epoch+1}, accuracy: {accuracy:.4f}")
+            # remember best acc and save checkpoint
+            is_best = accuracy > best_acc1
+            best_acc1 = max(accuracy, best_acc1)
+            print(f"=> Epoch: {epoch+1}, isBest? : {is_best}")
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'arch': args.arch,
+                'accuracy': accuracy,
+                'state_dict': model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+            }, is_best, save_dir=args.checkpoint_dir, epoch=(epoch+1), filename=os.path.join(args.checkpoint_dir, 'checkpoint_{:03d}.pth.tar'.format(epoch+1)))
+
+
+def train(train_loader, model, criterion, optimizer, epoch, args):
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    progress = ProgressMeter(
+        len(train_loader),
+        [batch_time, data_time, losses],
+        prefix="Epoch: [{}]".format(epoch+1))
+
+    """
+    Switch to eval mode:
+    Under the protocol of linear classification on frozen features/models,
+    it is not legitimate to change any part of the pre-trained model.
+    BatchNorm in train mode may revise running mean/std (even if it receives
+    no gradient), which are part of the model parameters too.
+    """
+    model.eval()
+
+    end = time.time()
+    for i, (images, target) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        images = images.cuda(args.gpu, non_blocking=True)
+        target = target.cuda(args.gpu, non_blocking=True)
+
+        # compute output
+        output = model(images)
+        loss = criterion(output, target)
+
+        # update loss record
+        losses.update(loss.item(), images.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            progress.display(i)
+
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """Decay the learning rate based on schedule"""
+    lr = args.lr
+    for milestone in args.schedule:
+        lr *= 0.1 if epoch >= milestone else 1.
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def evaluate(eval_loader, model, args):
+    model.eval()
+
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in eval_loader:
+            images, labels = data
+            images = images.cuda(args.gpu, non_blocking=True)
+            labels = labels.cuda(args.gpu, non_blocking=True)
+
+            output = model(images)
+            _, predicted = torch.max(output.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    return (100 * correct  / total)
+
+
+def save_checkpoint(state, is_best, save_dir, epoch, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    best_checkpoint = os.path.join(
+        save_dir, 'model_best_{:03d}.pth'.format(epoch))
+    if is_best:
+        sub_state = state['state_dict']
+        torch.save(sub_state, os.path.join(save_dir, 'model_sub.pth'))
+        shutil.copyfile(filename, best_checkpoint)
+
+
+def sanity_check(state_dict, pretrained_weights):
+    """
+    Linear classifier should not change any weights other than the linear layer.
+    This sanity check asserts nothing wrong happens (e.g., BN stats updated).
+    """
+    print("=> loading '{}' for sanity check".format(pretrained_weights))
+    checkpoint = torch.load(pretrained_weights, map_location="cpu")
+    state_dict_pre = checkpoint['state_dict']
+
+    for k in list(state_dict.keys()):
+        # only ignore fc layer
+        if 'fc.weight' in k or 'fc.bias' in k:
+            continue
+
+        # name in pretrained model
+        k_pre = 'encoder_q.' + k
+
+        assert ((state_dict[k].cpu() == state_dict_pre[k_pre]).all()), \
+            '{} is changed in linear classifier training.'.format(k)
+
+    print("=> sanity check passed.")
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+
+if __name__ == '__main__':
+    main()
